064f6a799c7ed2b3fcb6e51b39b388c5a8611cdf
Edwin Kolotilov <73223160+edwinn27@users.noreply.github.com>
2025-10-11 21:34:49 +0200

    Update arb_bot.py

--- arb_bot.py
+++ arb_bot.py
@@ -13,9 +13,9 @@ BASE_WALLET = os.environ.get("BASE_WALLET")
 SOLANA_WALLET = os.environ.get("SOLANA_WALLET")
 
 BASE_AMOUNT_ETH = Decimal("2.0")
-PROFIT_THRESHOLD_ETH = Decimal("0.0025")
+PROFIT_THRESHOLD_ETH = Decimal("0.02")
 POLL_INTERVAL = 30.0
-MAYAN_PROFIT_THRESHOLD_ETH = Decimal("0.004")  # wyższy próg dla Mayan
+MAYAN_PROFIT_THRESHOLD_ETH = Decimal("0.02")  # wyższy próg dla Mayan
 
 # LI.FI chain IDs
 FROM_CHAIN = 8453                   # Base
